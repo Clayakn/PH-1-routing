26428ab759819a78a8a07c69fa8e279540022253
Andrew  Nam <andrewnam@Andrews-MacBook-Pro.local>
2018-07-03 12:13:40 -0700

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import Home from './Home/Home';
+import About from './About/About';
+import FAQ from './About/FAQ/FAQ';
+import Company from './About/Company/Company';
+import { Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         This is App.js
+        <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        </nav>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' render={() => <About>
+            <Route path='/about/faq' component={FAQ}/>
+            <Route path='/about/company' component={Company}/> 
+          </About> } />
       </div>
     );
   }
